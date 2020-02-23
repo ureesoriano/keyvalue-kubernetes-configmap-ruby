2fd9ac1838828468153bc6ba1c503bdc8468b8df
Oriol Soriano <oriol.soriano@capside.com>
2020-02-23 19:36:51 +0100

    Add support for in-cluster authentication

--- lib/keyvalue/kubernetes/configmap/kubernetes_client_factory.rb
+++ lib/keyvalue/kubernetes/configmap/kubernetes_client_factory.rb
@@ -6,14 +6,33 @@ module KeyValue
       module KubernetesClientFactory
 
         def create_client(path_to_kubeconfig_file = nil)
-          config = Kubeclient::Config.read(path_to_kubeconfig_file || ENV['KUBECONFIG'])
+          if path_to_kubeconfig_file or ENV['KUBECONFIG']
+            config = Kubeclient::Config.read(path_to_kubeconfig_file || ENV['KUBECONFIG'])
 
-          Kubeclient::Client.new(
-            config.context.api_endpoint,
-            'v1',
-            ssl_options: config.context.ssl_options,
-            auth_options: config.context.auth_options,
-          )
+            Kubeclient::Client.new(
+              config.context.api_endpoint,
+              'v1',
+              ssl_options: config.context.ssl_options,
+              auth_options: config.context.auth_options,
+            )
+          else
+            # in-cluster authentication
+            auth_options = {
+              bearer_token_file: '/var/run/secrets/kubernetes.io/serviceaccount/token'
+            }
+            ssl_options = {}
+            if File.exist?("/var/run/secrets/kubernetes.io/serviceaccount/ca.crt")
+              ssl_options[:ca_file] = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+            end
+
+            Kubeclient::Client.new(
+              'https://kubernetes.default.svc',
+              'v1',
+              auth_options: auth_options,
+              ssl_options:  ssl_options
+            )
+          end
+
         end
       end
     end
